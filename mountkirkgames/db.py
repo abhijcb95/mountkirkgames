6392e33a462dac963d81101e59e5f3bc6cbd2e06
Abhinav <abhi_jcb@hotmail.com>
2021-01-09 15:52:13 +0800

    added firestore python script

---
+++ mountkirkgames/db.py
@@ -0,0 +1,22 @@
+import requests, json, os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+projectid = os.environ.get('DEVSHELL_PROJECT_ID')
+firebase_admin.initialize_app(cred, {
+  'projectId': projectid,
+})
+
+db = firestore.client()
+
+response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=7cd29c75593445baa0ae7068c297c1bc")
+response = json.loads(response.content.decode("utf-8"))
+
+hour = int(response['timezone']['current_time'][0:2]) - response['timezone']['gmt_offset']
+time = str(hour) + response['timezone']['current_time'][2:]
+
+doc_ref = db.collection(u'users').document(response['country_code'] + " " + time)
+doc_ref.set(response)
