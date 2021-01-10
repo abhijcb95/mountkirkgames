37e0b437fa89402eda5705e4ac23796fec82bcee
Abhinav <abhi_jcb@hotmail.com>
2021-01-10 13:27:42 +0800

    included pubsub module into db.py

--- mountkirkgames/db.py
+++ mountkirkgames/db.py
@@ -2,10 +2,11 @@ import requests, json, os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from google.cloud import pubsub_v1
 
 # Use the application default credentials
 cred = credentials.ApplicationDefault()
-projectid = os.environ.get('DEVSHELL_PROJECT_ID')
+project_id = os.environ.get('DEVSHELL_PROJECT_ID')
 firebase_admin.initialize_app(cred, {
   'projectId': projectid,
 })
@@ -24,6 +25,18 @@ elif hour > 24:
 
 time = str(hour) + response['timezone']['current_time'][2:]
 
-
+# inserting into firestore
 doc_ref = db.collection(u'users').document(response['country_code'] + " @ " + time + " GMT")
 doc_ref.set(response)
+
+#inserting into pubsub topic "backend-server-to-bq"
+publisher = pubsub_v1.PublisherClient()
+topic_id = "backend-server-to-bq"
+topic_path = publisher.topic_path(project_id, topic_id)
+message = {
+  "ip_address": response['ip_address'],
+  "longitude": response["longitude"],
+  "latitude": response['latitude'],
+  "GMT_time": time
+}
+future = publisher.publish(topic_path, json.dumps(message).encode("utf-8"))
