c3c303ab49d7452fa2f7048736327fd1d7da2d51
Abhinav <abhi_jcb@hotmail.com>
2021-01-13 00:55:56 +0800

    modified deployment.py after testing

--- deployment.py
+++ deployment.py
@@ -1,52 +1,57 @@
 import os
+import time
+import datetime
 
 def check_for_deployment_completion():
-    os.system("export DEPLOYMENT_COMPLETE=")
     os.system("export DEPLOYMENT_COMPLETE=$(gcloud deployment-manager deployments list | grep backend-services | grep DONE)")
-    DEPLOYMENT_COMPLETE = os.environ.get("DEPLOYMENT_COMPLETE")
+    return os.environ.get("DEPLOYMENT_COMPLETE")
 
-    while len(DEPLOYMENT_COMPLETE) == 0:
-        os.system("export DEPLOYMENT_COMPLETE=$(gcloud deployment-manager deployments list | grep backend-services | grep DONE)")
-        DEPLOYMENT_COMPLETE = os.environ.get("DEPLOYMENT_COMPLETE")
+def break_print(i):
+    time.sleep(i)
+    print("-----")
+    return 0
 
-    return True
+#Static IP
+static_ip = '35.244.246.143'
 
 #check for GCS_BUCKET value
 if not os.environ.get("GCS_BUCKET"):
-    gcs_bucket = "gcs-to-bq"
+    gcs_bucket = "gcs-to-bqs"
 else:
     gcs_bucket = os.environ.get("GCS_BUCKET")
-
-# create network, subnets, firewalls
-os.system("gcloud deployment-manager deployments create setup --config=dep-mgr/setup_deployment.yaml")
-
-continue_deployment = False
-while not continue_deployment:
-    print(" 2 Pre-requesites are required here. \n 1. Cloud router and Cloud Nat must be set up for us-central1, europe-west6 and asia-east2. \n 2. A golden image named 'image-backend-server' must be created. \n Type y once completed (ctrl + z to exit deployment)")
-    response = input()
-    if response == "y":
-        continue_deployment = True
-
-print("Setup deployment is completed, deploying the backend services now...")
 
 # instance template, healthcheck (w/ firewall), MIG, backend service, global lb
 # pubsub, bq
 os.system("gcloud deployment-manager deployments create backend-services --config=dep-mgr/backend_deployment.yaml")
 
+break_print(2)
+print("Deployment completed, starting dataflow streaming job...")
+
 # Start streaming job
-deployment_complete = False
+deployment_complete = None
+os.system("export DEPLOYMENT_COMPLETE=")
 while not deployment_complete:
     deployment_complete = check_for_deployment_completion()
 os.system("gcloud dataflow jobs run us-pubsub-to-bq --gcs-location gs://dataflow-templates-us-central1/latest/PubSub_to_BigQuery --region us-central1 --max-workers 3 --num-workers 1 --worker-machine-type n1-standard-1 --staging-location gs://{gcs_bucket}/temp/ --subnetwork https://www.googleapis.com/compute/v1/projects/g-grp4-implementation/regions/us-central1/subnetworks/us-subnet-data-analytics --network vpc-global --disable-public-ips --parameters inputTopic=projects/g-grp4-implementation/topics/backend-server-to-bq,outputTableSpec=g-grp4-implementation:pubsub_to_bq.server_data".format(gcs_bucket=gcs_bucket))
 
+break_print(2)
 print("Backend services deployment is completed, dataflow streaming job has started...")
 
-print("----")
-print("The server might take some time to be ready, test it out at 35.244.246.143")
-print(" ")
-print("After the server is ready and serving the game website, you can test the batch processing job with the following command: ")
-print("gcloud dataflow jobs run us-gcs-to-bq --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_BigQuery --region us-central1 --max-workers 3 --num-workers 1 --worker-machine-type n1-standard-1 --staging-location gs://{gcs_bucket}/temp --subnetwork https://www.googleapis.com/compute/v1/projects/g-grp4-implementation/regions/us-central1/subnetworks/us-subnet-data-analytics --network vpc-global --disable-public-ips --parameters javascriptTextTransformGcsPath=gs://{gcs_bucket}/dataflow_scripts/transform.js,JSONPath=gs://{gcs_bucket}/dataflow_scripts/bq-schema.json,javascriptTextTransformFunctionName=transform,outputTable=g-grp4-implementation:gcs_to_bq.user_files,inputFilePattern=gs://{gcs_bucket}/*.txt,bigQueryLoadingTemporaryDirectory=gs://{gcs_bucket}/temp".format(gcs_bucket=gcs_bucket))
-print("----")
-print("Open README.txt to see EXPECTED RESULTS in various sinks")
-print("----")
+break_print(5)
+print("The server might take some time to be ready")
+break_print(5)
+site_up = None
+while not site_up:
+    os.system("export SITE_UP=$(curl {static_ip} | grep Astray)".format(static_ip=static_ip))
+    site_up = os.environ.get("SITE_UP")
+    print("The site is not ready yet. Hang tight!")
+    time.sleep(20)
+now = datetime.datetime.utcnow().strftime('%H:%M:%S')
+print("The site is up at {static_ip}! Dataflow Batch Job will now start.".format(static_ip))
+
+os.system("gcloud dataflow jobs run us-gcs-to-bq --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_BigQuery --region us-central1 --max-workers 3 --num-workers 1 --worker-machine-type n1-standard-1 --staging-location gs://{gcs_bucket}/temp --subnetwork https://www.googleapis.com/compute/v1/projects/g-grp4-implementation/regions/us-central1/subnetworks/us-subnet-data-analytics --network vpc-global --disable-public-ips --parameters javascriptTextTransformGcsPath=gs://{gcs_bucket}/dataflow_scripts/transform.js,JSONPath=gs://{gcs_bucket}/dataflow_scripts/bq-schema.json,javascriptTextTransformFunctionName=transform,outputTable=g-grp4-implementation:gcs_to_bq.user_files,inputFilePattern=gs://{gcs_bucket}/*.txt,bigQueryLoadingTemporaryDirectory=gs://{gcs_bucket}/temp".format(gcs_bucket=gcs_bucket))
+break_print(2)
+print("All test components have completed.")
+print("Open README.txt to see EXPECTED RESULTS in various sinks, with GMT_time (approx.): {now}".format(now=now))
+break_print(5)
 print("This demo is completed, run 'python end_deployment.py' to delete all the resources.")
